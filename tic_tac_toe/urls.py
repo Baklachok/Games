7642d2c65e0fed7d4df32acb37c76da976c8b4f8
Даниил Коробко <korobko_01@inbox.ru>
2024-01-27 21:43:38 +0300

    added login-view

--- tic_tac_toe/urls.py
+++ tic_tac_toe/urls.py
@@ -4,7 +4,7 @@ from tic_tac_toe import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login_view, name="login"),
     path("sign_up", views.signup, name="sign_up"),
     path("logout", views.log_out, name="logout"),
 ]
--- tic_tac_toe/views.py
+++ tic_tac_toe/views.py
@@ -1,12 +1,22 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # Аутентификация пользователя
+            login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
 
 def signup(request):
     return render(request, 'registration.html')
